c516e6c87697a49a7de20aaa151dbf35604f6b2e
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-08-23 23:16:26 +0600

    adding imports

---
+++ tool.py
@@ -0,0 +1,7 @@
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+import logging
+#visualization
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+from sklearn.manifold import TSNE
