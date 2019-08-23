cf9ad048f748e123138fb182aa4cae93c8b1f03e
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-08-23 23:39:10 +0600

    tokenizing text : #1

--- tool.py
+++ tool.py
@@ -5,3 +5,15 @@ import logging
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 from sklearn.manifold import TSNE
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+
+
+text = "প্রায় ১৬৭ বছরের পুরোনো এই বিদ্যালয়টি বিশ্ববিদ্যালয়ের অংশ হলে এর ঐতিহ্য বিপন্ন হবে বলে তাদের আশঙ্কা।"
+marked_text = "[CLS] " + text + " [SEP]"
+print (marked_text)
+
+re.sub('।', '', marked_text)
+
+tokenized_text = tokenizer.tokenize(marked_text)
+print (tokenized_text)
